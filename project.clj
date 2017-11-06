5133e6b2330ee809738b5e3b3afefe3816b5e8cf
carcigenicate <slomifier@gmail.com>
2017-11-06 14:44:18 -0700

    Initial commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject flood "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [seesaw "1.4.5"]
+                 [helpers "1"]]
+
+  :main ^:skip-aot flood.main
+
+  :target-path "target/%s"
+
+  :profiles {:uberjar {:aot :all}})
